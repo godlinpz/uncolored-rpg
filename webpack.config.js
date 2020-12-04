462b2264737746382b983dd3e844a8b0159f5973
Dmitry Pavlichenko <godlin.pz@gmail.com>
2020-12-04 20:55:08 +0300

    Project setup

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+const NODE_ENV = process.env.NODE_ENV;
+
+module.exports = {
+    resolve: {
+        extensions: ['.js', '.jsx', '.json'],
+    },
+    mode: NODE_ENV || 'development',
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+    },
+    watch: NODE_ENV === 'development',
+    watchOptions: {
+        ignored: /node_modules/,
+        poll: 1000,
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: [/node_modules/],
+            },
+            {
+                test: /\.(c|sa|sc)ss/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 3,
+                            modules: {
+                                mode: 'local',
+                                localIdentName: '[name]__[local]__[hash:base64:5]',
+                                auto: /\.module.\.\w+$/i
+                            }
+                        }
+                    },
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, 'public/index.html')
+        })
+    ],
+
+    devServer: {
+        port: 3002,
+        overlay: true,
+        open: true,
+        hot: true,
+        historyApiFallback: true,
+    },
+
+    devtool: 'source-map',
+}
